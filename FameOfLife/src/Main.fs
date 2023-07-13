d8d53feb96d2d288901d4b76ebf31cda227696e0
skywalker <j.karlsson@retrocoder.se>
2023-07-13 11:50:17 +0200

    Added initial Visual Studio F# project

---
+++ FameOfLife/src/Main.fs
@@ -0,0 +1,26 @@
+// FameOfLife : Copyright (C) 2023 Skywalker<j.karlsson@retrocoder.se>
+module Main
+
+[<EntryPoint>]
+let main argv =
+    printfn "FameOfLife - Conway's Game of life written in F#"
+    0
+
+
+// License
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.
+// If not, see <http://www.gnu.org/licenses/> or write to the
+// Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor,
+// Boston, MA 02110-1301
+// USA
