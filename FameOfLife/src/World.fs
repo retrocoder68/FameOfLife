c3a5fe31b472da745371386d981c24df2f8a5812
skywalker <j.karlsson@retrocoder.se>
2023-07-15 13:52:44 +0200

    Try do improve performance by changing the internal representaion of a World from List to Array

--- FameOfLife/src/World.fs
+++ FameOfLife/src/World.fs
@@ -2,22 +2,24 @@
 module World
 
 type World(width:int, height:int) =
-    let mutable _cells:List<bool> = [for x in 0..width-1 do for y in 0..height-1 -> System.Random().Next(2) = 1]
+    let mutable _cells:bool[,] = Array2D.init width height (fun x y -> System.Random().Next(2) = 1)
 
     member val width:int = width
     member val height:int = height
     member this.cells with get() = _cells and set(value) = _cells <- value
 
     member this.update() =
-        let newCells = List.map (fun (x,y) ->
+        let newCells = Array2D.zeroCreate width height
+        List.map (fun (x,y) ->
             let neighborCount = (List.filter (fun b ->
                 b = true) (List.map (fun (xn,yn) ->
                     if xn >= 0 && xn < width && yn >= 0 && yn < height && (xn <> x || yn <> y) then
-                        this.cells.[xn + yn*width]
+                        this.cells.[xn, yn]
                     else
                         false) [for x in x-1..x+1 do for y in y-1..y+1 -> (x,y)])).Length
-            if this.cells.[x + y*width] then neighborCount = 2 || neighborCount = 3
-            else neighborCount = 3) [for y in 0..height-1 do for x in 0..width-1 -> (x,y)]
+            newCells[x,y] <-
+                if this.cells.[x, y] then neighborCount = 2 || neighborCount = 3
+                else neighborCount = 3) [for y in 0..height-1 do for x in 0..width-1 -> (x,y)] |> ignore
         this.cells <- newCells
 
 
